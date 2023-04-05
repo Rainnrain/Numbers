7cb4142a8b6e048820c1b9b4211ed1b99f20ffee
Lorraine <lorrainecannom@gmail.com>
2023-04-05 10:34:19 -0400

    Adding OPENAPI

--- rest-book/src/main/java/com/lorraine/Book.java
+++ rest-book/src/main/java/com/lorraine/Book.java
@@ -1,14 +1,27 @@
 package com.lorraine;
 
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import javax.json.bind.annotation.JsonbDateFormat;
+import javax.json.bind.annotation.JsonbProperty;
 import java.time.Instant;
+@Schema(description = "A book")
 
 public class Book {
-
+@JsonbProperty("isbn_13")
+@Schema(required = true)
     public String isbn13;
+    @Schema(required = true)
     public String title;
     public String author;
+    @JsonbProperty("year_of_publication")
     public int yearOfPublication;
     public String genre;
+
+    @JsonbDateFormat("yyyy/MM/dd")
+    @JsonbProperty("creation_date")
+    @Schema(implementation = String.class, format="date")
     public Instant creationDate;
 
     @Override
--- rest-book/src/main/java/com/lorraine/BookResource.java
+++ rest-book/src/main/java/com/lorraine/BookResource.java
@@ -1,5 +1,7 @@
 package com.lorraine;
 
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
@@ -10,14 +12,19 @@ import java.time.Instant;
 
 
 @Path("/api/books")
+@Tag(name="Book REST endpoint")
 public class BookResource {
 
     @Inject
     Logger logger;
 
-    @GET
+    @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Operation(
+            summary="Creates a Book",
+            description = "Creates a Book with an ISBN number"
+    )
     public Response createABook(@FormParam("title") String title, @FormParam("author") String author,@FormParam("year") int yearOfPublication, @FormParam("genre")String genre){
         Book book= new Book();
         book.isbn13="we will get it later from the Number Microservice";
